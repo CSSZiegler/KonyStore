469a2c86bb7be457a391bf6be1436cae969389f9
bhargavib <bhargavi.burugupalli@kony.com>
2013-04-22 19:40:07 -0400

    new module for preshow functions

---
+++ modules/js/preShowLogic.js
@@ -0,0 +1,62 @@
+/*
+****************************************************************
+*	Name    : preShowHome
+*	Author  : Kony Solutions
+*	Purpose : This function is preshow for Home to handle the form pre show logic.
+****************************************************************
+*/
+function preShowHome(){
+	hbxImg.setVisibility(true);
+	hbxHeaderBack.setVisibility(false);
+	hbxSearch.setVisibility(true);
+	hbxSearch.txtbxSrch.text="";
+}
+
+/*
+****************************************************************
+*	Name    : preShowProduct
+*	Author  : Kony Solutions
+*	Purpose : This function is common preshow for forms with Back button to handle the form pre show logic.
+****************************************************************
+*/
+function preShowProduct(){
+	hbxImg.setVisibility(false);
+	hbxHeaderBack.setVisibility(true);
+	hbxSearch.setVisibility(false);
+}
+
+/*
+****************************************************************
+*	Name    : preShowSubcat
+*	Author  : Kony Solutions
+*	Purpose : This function is common preshow for forms with Back button to handle the form pre show logic.
+****************************************************************
+*/
+function preShowSubCat(){
+	hbxImg.setVisibility(false);
+	hbxHeaderBack.setVisibility(true);
+	hbxSearch.setVisibility(true);
+	hbxSearch.txtbxSrch.text="";
+}
+
+
+/*
+****************************************************************
+*	Name    : preShowProdDetails
+*	Author  : Kony Solutions
+*	Purpose : This function is preshow for product details to handle the form pre show logic.
+****************************************************************
+*/
+
+function preShowProdDetails(){
+	hbxSearch.setVisibility(false); //setting the search hbox in the form header to false
+	hbxHeaderBack.btnHeaderBack.setVisibility(true);
+	if(frmProdDetails.lblReview.text=="")
+	{
+		frmProdDetails.lblReview.text="No Reviews";	
+		frmProdDetails.imgReview.setVisibility(false);
+	}	
+	if( frmProdDetails.lblDesc.text == undefined)
+	frmProdDetails.lblDesc.text = "";
+}
+
